994f01fc0a1c5d354ef617d4d6f8ae2a01d2a18c
Kyril Jardinico <Kyril.Jay@gmail.com>
2021-03-13 21:12:09 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,41 @@
+/* JavaScipt function expressions for calculating volumes of 3-D shapes. */
+
+/* Sphere. V = (4/3) × π × r3  */
+const sphereVolume = (radius) => {
+    let radiusCubed = Math.pow(radius, 3);
+    return 4/3 * Math.PI  * radiusCubed;
+}
+
+
+
+/* Cone.  V = 1/3 × π × r2 × h. */
+const coneVolume = (radius, height) => {
+    let radiusSquared = Math.pow(radius, 2);
+    return 1/3 * Math.PI * radiusSquared * height;
+}
+
+/* Cylinder. V = π x r2 x h. */
+const cylinderVolume = (radius, height) => {
+    let radiusSquared = Math.pow(radius, 2);
+    return Math.PI * radiusSquared * height;
+}
+
+
+/* Cube. V = S3. */
+const cubeVolume = (side) => {
+    let sideCubed = Math.pow(side, 3);
+    return sideCubed;
+}
+
+
+/* Cuboid. V = l x h x w. */
+const cuboidVolume = (length, height, width) => {
+    return length * height * width;
+}
+
+/* Square Pyramid. V = 1/3 x b2 x h. */
+const squarePyramidVolume = (base, height) => {
+    let baseSquared = Math.pow(base, 2);
+    return 1/3 * baseSquared * height;
+}
+
